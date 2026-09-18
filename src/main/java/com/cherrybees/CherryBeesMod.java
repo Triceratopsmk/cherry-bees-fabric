8a4a787e97e696fe859df186d980e3840347625e
Triceratopsmk <triceratops.mk@gmail.com>
2026-09-18 19:09:58 +0300

    Update CherryBeesMod.java

--- src/main/java/com/cherrybees/CherryBeesMod.java
+++ src/main/java/com/cherrybees/CherryBeesMod.java
@@ -4,11 +4,13 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SpawnEggItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,14 +19,17 @@ public class CherryBeesMod implements ModInitializer {
     public static final String MOD_ID = "cherrybees";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    private static final RegistryKey<ItemGroup> SPAWN_EGGS_GROUP =
+            RegistryKey.of(RegistryKeys.ITEM_GROUP, Identifier.of("minecraft", "spawn_eggs"));
+
     public static final Item CHERRY_BEE_SPAWN_EGG = Registry.register(
             Registries.ITEM,
             Identifier.of(MOD_ID, "cherry_bee_spawn_egg"),
-            new SpawnEggItem(EntityType.BEE, new Item.Settings()));
+            new SpawnEggItem(new Item.Settings().spawnEgg(EntityType.BEE)));
 
     @Override
     public void onInitialize() {
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(entries -> {
+        ItemGroupEvents.modifyEntriesEvent(SPAWN_EGGS_GROUP).register(entries -> {
             entries.add(new ItemStack(CHERRY_BEE_SPAWN_EGG));
         });
 
