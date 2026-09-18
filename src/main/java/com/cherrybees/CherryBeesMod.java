7b1bdd06b0c87a61108ecbb80a70140c426329ef
Triceratopsmk <triceratops.mk@gmail.com>
2026-09-18 19:00:25 +0300

    Update CherryBeesMod.java

--- src/main/java/com/cherrybees/CherryBeesMod.java
+++ src/main/java/com/cherrybees/CherryBeesMod.java
@@ -1,6 +1,15 @@
 package com.cherrybees;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.entity.EntityType;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SpawnEggItem;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,9 +17,17 @@ public class CherryBeesMod implements ModInitializer {
     public static final String MOD_ID = "cherrybees";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    public static final Item CHERRY_BEE_SPAWN_EGG = Registry.register(
+            Registries.ITEM,
+            Identifier.of(MOD_ID, "cherry_bee_spawn_egg"),
+            new SpawnEggItem(EntityType.BEE, new Item.Settings()));
+
     @Override
     public void onInitialize() {
-        LOGGER.info("Cherry Bees loaded - bees now pollinate cherry blossoms!");
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(entries -> {
+            entries.add(new ItemStack(CHERRY_BEE_SPAWN_EGG));
+        });
+
+        LOGGER.info("Cherry Bees loaded - pink bees pollinate cherry blossoms!");
     }
 }
-
