b1c8c9581a6ee4b257505a93ebb5ba22f74df374
Triceratopsmk <triceratops.mk@gmail.com>
2026-09-18 20:26:46 +0300

    Create GiantStatusEffect.java

---
+++ src/main/java/com/cherrybees/GiantStatusEffect.java
@@ -0,0 +1,11 @@
+package com.cherrybees;
+
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+
+public class GiantStatusEffect extends StatusEffect {
+    public GiantStatusEffect(StatusEffectCategory category, int color) {
+        super(category, color);
+    }
+}
+
