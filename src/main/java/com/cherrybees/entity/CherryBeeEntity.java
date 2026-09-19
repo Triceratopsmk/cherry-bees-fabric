ab781acb2fb1ef9384a712a0c26207159ac1da33
Triceratopsmk <triceratops.mk@gmail.com>
2026-09-19 10:20:09 +0300

    Update CherryBeeEntity.java

--- src/main/java/com/cherrybees/entity/CherryBeeEntity.java
+++ src/main/java/com/cherrybees/entity/CherryBeeEntity.java
@@ -1,12 +1,41 @@
 package com.cherrybees.entity;
 
+import com.cherrybees.CherryBeesMod;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.entity.ai.goal.PrioritizedGoal;
 import net.minecraft.entity.passive.BeeEntity;
+import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CherryBeeEntity extends BeeEntity {
     public CherryBeeEntity(EntityType<? extends BeeEntity> entityType, World world) {
         super(entityType, world);
     }
-}
 
+    @Override
+    protected void initGoals() {
+        super.initGoals();
+        List<Goal> toRemove = new ArrayList<>();
+        for (PrioritizedGoal prioritizedGoal : this.goalSelector.getGoals()) {
+            String name = prioritizedGoal.getGoal().getClass().getSimpleName();
+            if (name.equals("PollinateGoal") || name.equals("MoveToFlowerGoal") || name.equals("ValidateFlowerGoal")) {
+                toRemove.add(prioritizedGoal.getGoal());
+            }
+        }
+        for (Goal goal : toRemove) {
+            this.goalSelector.remove(goal);
+        }
+        this.goalSelector.add(5, new CherryPollinateGoal(this));
+    }
+
+    @Override
+    public CherryBeeEntity createChild(ServerWorld world, PassiveEntity entity) {
+        return CherryBeesMod.CHERRY_BEE.create(world, SpawnReason.BREEDING);
+    }
+}
