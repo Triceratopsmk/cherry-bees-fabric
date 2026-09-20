5eb0a3922fcccbc28384c92aadaf821ac2e2b661
Triceratopsmk <triceratops.mk@gmail.com>
2026-09-20 13:05:16 +0300

    Update CherryBeesModClient.java

--- src/main/java/com/cherrybees/CherryBeesModClient.java
+++ src/main/java/com/cherrybees/CherryBeesModClient.java
@@ -1,6 +1,7 @@
 package com.cherrybees;
 
 import com.cherrybees.client.CherryBeeEntityRenderer;
+import com.cherrybees.client.WildBoarEntityRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 
@@ -8,6 +9,6 @@ public class CherryBeesModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         EntityRendererRegistry.register(CherryBeesMod.CHERRY_BEE, CherryBeeEntityRenderer::new);
+        EntityRendererRegistry.register(CherryBeesMod.WILD_BOAR, WildBoarEntityRenderer::new);
     }
 }
-
